5498388e91627f5f87018f1471f19d1c1e665d95
XINYI XU <xinyixu@XINYIs-MacBook-Pro.local>
2018-10-20 17:23:49 +1100

    Add app header

--- rocketgo/src/App.js
+++ rocketgo/src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import MapPage from "./Map/MapPage";
-import { Button } from "semantic-ui-react";
+import MenuHeader from './Header';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <MenuHeader/>
                 <MapPage/>
             </div>
         );
