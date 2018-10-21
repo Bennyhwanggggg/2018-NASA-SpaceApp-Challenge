c046dddcdcf929fc9de50dd5fc6faf468a659b11
XINYI XU <xinyixu@XINYIs-MacBook-Pro.local>
2018-10-21 11:09:18 +1100

    Change data structure to be space center based

--- rocketgo/src/store/store.js
+++ rocketgo/src/store/store.js
@@ -2,29 +2,45 @@ const store = {
     zoomLocation: undefined,
     missions: [
         {
-            missionCode: 'Hellas Sat 4/SaudiGeoSat-1',
+            launchCenter: 'French Space Center',
             coordinates: [8.5417, 47.3769],
             remainDays: 15,
-            launchDetail: {
-                rocket: 'Ariane 5 ECA',
-                agency: 'Arianespace',
-                date: '31/10/2018',
-                location: 'Kourou, French Guiana',
-                missionDetails: 'Top Secret!!!!!!!!!!'
-            }
+            launchDetail: [
+                {
+                    rocket: 'Ariane 5 ECA',
+                    missionCode: 'Hellas Sat 4/SaudiGeoSat-1',
+                    agency: 'Arianespace',
+                    date: '31/10/2018',
+                    location: 'Kourou, French Guiana',
+                    missionDetails: 'Top Secret!!!!!!!!!!'
+                },
+                {
+                    rocket: 'Ariane 5 ECA',
+                    missionCode: 'EDRS-C/HYLAS 3',
+                    agency: 'Arianespace',
+                    date: '30/11/2018',
+                    location: 'Kourou, French Guiana',
+                    missionDetails: 'Top Secret!!!!!!!!!!'
+                },
+                {
+                    rocket: 'Ariane 5 ECA',
+                    missionCode: 'GEO-KOMPSAT-2A',
+                    agency: 'Arianespace',
+                    date: '30/11/2018',
+                    location: 'Kourou, French Guiana',
+                    missionDetails: 'Top Secret!!!!!!!!!!'
+                },
+                {
+                    rocket: 'Ariane 5 ECA',
+                    missionCode: 'GSAT-31',
+                    agency: 'Arianespace',
+                    date: '30/11/2018',
+                    location: 'Kourou, French Guiana',
+                    missionDetails: 'Top Secret!!!!!!!!!!'
+                },
+            ]
         },
-        {
-            missionCode: 'GSAT-29',
-            coordinates: [80.2304, 13.7199],
-            remainDays: 40,
-            launchDetail: {
-                rocket: 'Ariane 5 ECA',
-                agency: 'Arianespace',
-                date: '31/10/2018',
-                location: 'Kourou, French Guiana',
-                missionDetails: 'Top Secret!!!!!!!!!!'
-            }
-        },
+
     ],
 };
 
